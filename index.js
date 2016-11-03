e00d96180385655946d208a43a60a20658d85934
harmonicsf <harmonic.sf@gmail.com>
2016-11-04 05:22:24 +0700

    asd

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var browserify = require('browserify');
+var React = require('react');
+var jsx = require('node-jsx');
+var app = express();
+
+jsx.install();
+
+
+var Books = require('./views/index.jsx');
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+app.use('/bundle.js', function(req, res) {
+  res.setHeader('content-type', 'application/javascript');
+  browserify('./app.js', {
+    debug: true
+  })
+  .transform('reactify')
+  .bundle()
+  .pipe(res);
+});
+
+app.use('/', function(req, res) {
+  var books = [{
+    title: 'Professional Node.js',
+    read: false
+  }, {
+    title: 'Node.js Patterns',
+    read: false
+  }];
+
+  res.setHeader('Content-Type', 'text/html');
+  res.end(React.renderToStaticMarkup(
+    React.DOM.body(
+      null,
+      React.DOM.div({
+        id: 'container',
+        dangerouslySetInnerHTML: {
+          __html: React.renderToString(React.createElement(Books, {
+            books: books
+          }))
+        }
+      }),
+      React.DOM.script({
+        'id': 'initial-data',
+        'type': 'text/plain',
+        'data-json': JSON.stringify(books)
+      }),
+      React.DOM.script({
+        src: '/bundle.js'
+      })
+    )
+  ));
+});
+
+var server = app.listen(port, ip, function() {
+  var addr = server.address();
+  console.log('Listening @ http://%s:%d', addr.address, addr.port);
+});
